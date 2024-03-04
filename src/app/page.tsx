19d19921e6c21a9dd5b5b212f24b28a954ad3ca6
Aiden X <aiden@toriai.com>
2024-03-04 13:27:46 +0800

    fix: update whitepaper link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import "animate.css";
 import "./page.scss";
+import Link from "next/link";
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -29,8 +30,7 @@ export default function Home() {
           homeServiceSubscribeRef.current.style.opacity = "1";
         }
         setShowFrame(3);
-        const list: NodeListOf<HTMLElement> =
-          document.querySelectorAll(".first-invisible");
+        const list: NodeListOf<HTMLElement> = document.querySelectorAll(".first-invisible");
         Array.from(list || []).forEach((item: HTMLElement) => {
           // item.style.opacity = "1";
           item.classList.add("animate__animated", "animate__fadeInUp");
@@ -59,8 +59,7 @@ export default function Home() {
               homeServiceSubscribeRef.current.style.opacity = "1";
             }
 
-            const list: NodeListOf<HTMLElement> =
-              document.querySelectorAll(".first-invisible");
+            const list: NodeListOf<HTMLElement> = document.querySelectorAll(".first-invisible");
             Array.from(list || []).forEach((item: HTMLElement) => {
               item.classList.add("animate__animated", "animate__fadeInUp");
               // item.style.opacity = "1";
@@ -72,13 +71,7 @@ export default function Home() {
   }, []);
   return (
     <div className="home__wrapper">
-      <video
-        className="home__first-video"
-        ref={videoRef}
-        src="/first.webm"
-        autoPlay={true}
-        muted={true}
-      ></video>
+      <video className="home__first-video" ref={videoRef} src="/first.webm" autoPlay={true} muted={true}></video>
       {/* <Image
         className="home__first-bg"
         width={1920}
@@ -86,13 +79,7 @@ export default function Home() {
         src="/first_bg.png"
         alt="bg"
       ></Image> */}
-      <Image
-        className="home__logo first-invisible"
-        width={243}
-        height={210}
-        src="/logo.png"
-        alt="logo"
-      ></Image>
+      <Image className="home__logo first-invisible" width={243} height={210} src="/logo.png" alt="logo"></Image>
       <div className="home__nav first-invisible">
         <div
           className="home__nav-item"
@@ -103,13 +90,7 @@ export default function Home() {
             }
           }}
         >
-          <Image
-            src="/products.png"
-            width={203}
-            height={43}
-            className="home__nav-item-img"
-            alt="products"
-          ></Image>
+          <Image src="/products.png" width={203} height={43} className="home__nav-item-img" alt="products"></Image>
           <Image
             src="/products-active.png"
             width={203}
@@ -118,14 +99,8 @@ export default function Home() {
             alt="products"
           ></Image>
         </div>
-        <div className="home__nav-item">
-          <Image
-            src="/whitepaper.png"
-            width={203}
-            height={43}
-            className="home__nav-item-img"
-            alt="products"
-          ></Image>
+        <Link href={"/whitepaperV0.1.0_0304.pdf"} target="_blank" className="home__nav-item">
+          <Image src="/whitepaper.png" width={203} height={43} className="home__nav-item-img" alt="products"></Image>
           <Image
             src="/whitepaper-active.png"
             width={203}
@@ -133,15 +108,9 @@ export default function Home() {
             className="home__nav-item-img--active"
             alt="products"
           ></Image>
-        </div>
+        </Link>
         <div className="home__nav-item">
-          <Image
-            src="/twitter.png"
-            width={203}
-            height={43}
-            className="home__nav-item-img"
-            alt="products"
-          ></Image>
+          <Image src="/twitter.png" width={203} height={43} className="home__nav-item-img" alt="products"></Image>
           <Image
             src="/twitter-active.png"
             width={203}
@@ -179,27 +148,9 @@ export default function Home() {
       <div className="home__hook" id="js__product">
         {showFrame > 1 ? (
           <div className="animate__animated animate__fadeInUp">
-            <Image
-              src="/bg2-1.png"
-              alt="service"
-              width={2873}
-              height={1440}
-              className="home__hook-bg1"
-            ></Image>
-            <Image
-              src="/bg2-2.png"
-              alt="service"
-              width={5760}
-              height={1512}
-              className="home__hook-bg2"
-            ></Image>
-            <Image
-              src="/bg2-3.png"
-              alt="service"
-              width={5760}
-              height={1512}
-              className="home__hook-bg3"
-            ></Image>
+            <Image src="/bg2-1.png" alt="service" width={2873} height={1440} className="home__hook-bg1"></Image>
+            <Image src="/bg2-2.png" alt="service" width={5760} height={1512} className="home__hook-bg2"></Image>
+            <Image src="/bg2-3.png" alt="service" width={5760} height={1512} className="home__hook-bg3"></Image>
             <video
               ref={video2Ref}
               className="home__hook-video"
@@ -210,10 +161,8 @@ export default function Home() {
             ></video>
             <div className="home__hook-content">
               <div className="home__hook-detail">
-                The world&apos;s first{" "}
-                <span>DePIN liquidity aggregation protocol</span>. Turning AI
-                computational resources into a new form of DeFi liquidity.{" "}
-                <br></br>
+                The world&apos;s first <span>DePIN liquidity aggregation protocol</span>. Turning AI computational
+                resources into a new form of DeFi liquidity. <br></br>
                 <br></br>
                 Get the maximum <span>DePIN mining</span> yield from us!
               </div>
@@ -225,20 +174,8 @@ export default function Home() {
       <div className="home__sdk">
         {showFrame > 2 ? (
           <div className="animate__animated animate__fadeInUp">
-            <Image
-              src="/bg3.png"
-              alt="sdk"
-              width={2135}
-              height={1023}
-              className="home__sdk-bg"
-            ></Image>
-            <video
-              ref={video3Ref}
-              className="home__sdk-video"
-              src="/third.webm"
-              autoPlay={true}
-              muted={true}
-            >
+            <Image src="/bg3.png" alt="sdk" width={2135} height={1023} className="home__sdk-bg"></Image>
+            <video ref={video3Ref} className="home__sdk-video" src="/third.webm" autoPlay={true} muted={true}>
               {/* <source src="/third.webm" type="video/webm"></source> */}
             </video>
             <div className="home__sdk-content">
@@ -249,40 +186,29 @@ export default function Home() {
                 <li>Edge & geo response time optimization</li>
               </ol>
               <div>
-                One SDK for all DePINs. <br></br>DePIN Development experience
-                streamlined.
+                One SDK for all DePINs. <br></br>DePIN Development experience streamlined.
               </div>
 
               <div className="home__sdk-soon">Coming soon...</div>
             </div>
             <div className="home__sdk-desc-box">
               <div>
-                We are a bunch of pro-human degens with experience working at
-                tech giants, top DeFi protocols, super well-funded Layer1 chain,
-                and crypto funds & trading prop shop, as founding team members,
-                senior managers, and C-levels. Graduated from top universities
-                etc etc. <br></br>
+                We are a bunch of pro-human degens with experience working at tech giants, top DeFi protocols, super
+                well-funded Layer1 chain, and crypto funds & trading prop shop, as founding team members, senior
+                managers, and C-levels. Graduated from top universities etc etc. <br></br>
                 <br></br>
-                However, please try to look beyond these labels on us. Labels
-                are shortcuts to establishing authority and building blind
-                trust, but they don&apos;t help us humans to bond and your
-                precious trust should not be given for free.<br></br>
+                However, please try to look beyond these labels on us. Labels are shortcuts to establishing authority
+                and building blind trust, but they don&apos;t help us humans to bond and your precious trust should not
+                be given for free.<br></br>
                 <br></br>
-                We encourage you to feel us, feel our passion, through our
-                products and words. We are human-made and are cooking smth smth
-                cool for humans, for you & your kids, for making democratizing
-                the future of compute a <span>dream come true</span>.🪄
+                We encourage you to feel us, feel our passion, through our products and words. We are human-made and are
+                cooking smth smth cool for humans, for you & your kids, for making democratizing the future of compute a{" "}
+                <span>dream come true</span>.🪄
               </div>
             </div>
             <div className="home__footer">
               <div className="home__footer-logo-box">
-                <Image
-                  className="home__footer-logo"
-                  width={243}
-                  height={210}
-                  src="/logo.png"
-                  alt="logo"
-                ></Image>
+                <Image className="home__footer-logo" width={243} height={210} src="/logo.png" alt="logo"></Image>
               </div>
               <div className="home__footer-nav">
                 <div className="home__footer-nav-item">
